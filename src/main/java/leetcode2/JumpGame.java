db310c2028890dcdf5ef7be27a80c74b5ce6b80e
iscdr <iscdr10@gmail.com>
2025-12-29 01:39:30 +0300

    Jump Game

---
+++ src/main/java/leetcode2/JumpGame.java
@@ -0,0 +1,13 @@
+package leetcode2;
+
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int farthest = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > farthest) return false;
+            farthest = Math.max(farthest, i + nums[i]);
+        }
+        return true;
+
+    }
+}
