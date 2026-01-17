f307892855b650df8de4b2b8b38fdbc6606fd430
iscdr <iscdr10@gmail.com>
2026-01-17 22:01:20 +0300

    Search a 2D Matrix

---
+++ src/main/java/leetcode2/Searcha2DMatrix.java
@@ -0,0 +1,27 @@
+package leetcode2;
+
+public class Searcha2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            int midValue = matrix[row][col];
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
