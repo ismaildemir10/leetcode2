57c924acf67b57ef96868c4051d00cf46cd179dd
iscdr <iscdr10@gmail.com>
2026-01-21 22:44:16 +0300

    Minimum Window Substring

--- src/main/java/leetcode2/MinimumWindowSubstring.java
+++ src/main/java/leetcode2/MinimumWindowSubstring.java
@@ -1,4 +1,52 @@
 package leetcode2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinimumWindowSubstring {
+    public String minWindow(String s, String t) {
+
+        if (s.length() < t.length()) return "";
+
+        Map<Character, Integer> need = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        Map<Character, Integer> window = new HashMap<>();
+        int left = 0, right = 0;
+        int valid = 0;
+        int start = 0, minLen = Integer.MAX_VALUE;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+
+            while (valid == need.size()) {
+                if (right - left < minLen) {
+                    start = left;
+                    minLen = right - left;
+                }
+                char d = s.charAt(left);
+                left++;
+
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+
+    }
 }
